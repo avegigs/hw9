a656cf34d26a4040fa2917ebf559e2d4b54d43c4
avegigs <125361614+avegigs@users.noreply.github.com>
2023-05-03 18:32:21 +0300

    Update hw9.py

--- hw9.py
+++ hw9.py
@@ -1,65 +1,101 @@
-def input_error(func):
-    def wrapper(*args, **kwargs):
+USERS = {}
+
+# decorator
+
+
+def error_handler(func):
+    def inner(*args):
         try:
-            return func(*args, **kwargs)
+            result = func(*args)
+            return result
         except KeyError:
-            return "Contact not found."
+            return "No user"
         except ValueError:
-            return "Invalid input format."
+            return 'Give me name and phone please'
         except IndexError:
-            return "Invalid command."
-    return wrapper
+            return 'Enter user name'
+    return inner
 
 
-def hello_command():
+def hello_user(_):
     return "How can I help you?"
 
 
-@input_error
-def add_command(user_input, contacts):
-    _, contact_name, phone_number = user_input.split()
-    contacts[contact_name] = phone_number
-    return "Contact added successfully."
+def unknown_command(_):
+    return "unknown_command"
 
 
-@input_error
-def change_command(user_input, contacts):
-    _, contact_name, phone_number = user_input.split()
-    contacts[contact_name] = phone_number
-    return "Phone number updated successfully."
+def exit(_):
+    return
 
 
-@input_error
-def phone_command(user_input, contacts):
-    _, contact_name = user_input.split()
-    return contacts[contact_name]
+@error_handler
+def add_user(args):
+    name, phone = args
+    USERS[name] = phone
+    return f'User {name} added!'
 
 
-def show_all_command(contacts):
-    if not contacts:
-        return "No contacts found."
-    return "\n".join(f"{contact}: {phone}" for contact, phone in contacts.items())
+@error_handler
+def change_phone(args):
+    name, phone = args
+    old_phone = USERS[name]
+    USERS[name] = phone
+    return f'У {name} тепер телефон: {phone} Старий номер: {old_phone}'
+
+
+def show_all(_):
+    result = ''
+    for name, phone in USERS.items():
+        result += f'Name: {name} phone: {phone}\n'
+    return result
+
+
+@error_handler
+def show_phone(args):
+    name, *_ = args
+    # print(name)
+    phone = USERS[name]
+    result = f'Phone: {phone} for user: {name}\n'
+    return result
+
+
+HANDLERS = {
+    'hello': hello_user,
+    'add': add_user,
+    'change': change_phone,
+    'show all': show_all,
+    'phone': show_phone,
+    'exit': exit,
+    'goodbye': exit,
+    'close': exit,
+}
+
+
+def parse_input(user_input):
+    command, *args = user_input.split()
+    command = command.lstrip()
+
+    try:
+        handler = HANDLERS[command.lower()]
+    except KeyError:
+        if args:
+            command = command + ' ' + args[0]
+            args = args[1:]
+        handler = HANDLERS.get(command.lower(), unknown_command)
+    return handler, args
 
 
 def main():
-    contacts = {}
     while True:
-        user_input = input("Enter a command: ").lower()
-        if user_input == "hello":
-            print(hello_command())
-        elif user_input.startswith("add"):
-            print(add_command(user_input, contacts))
-        elif user_input.startswith("change"):
-            print(change_command(user_input, contacts))
-        elif user_input.startswith("phone"):
-            print(phone_command(user_input, contacts))
-        elif user_input == "show all":
-            print(show_all_command(contacts))
-        elif user_input in ["good bye", "close", "exit"]:
-            print("Good bye!")
+        # example: add Petro 0991234567
+        user_input = input('Please enter command and args: ')
+        handler, *args = parse_input(user_input)
+        result = handler(*args)
+        if not result:
+            print('Exit')
             break
-        else:
-            print("Invalid command. Try again.")
+        print(result)
 
 
 if __name__ == "__main__":
